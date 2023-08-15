c9073cd3a20a39a28791fb9d184849097b84cc14
Richard Freeman <rich0@gentoo.org>
2023-08-15 11:15:42 -0400

    Add latest tag to build script.
    Signed-off-by: Richard Freeman <rich0@gentoo.org>

--- build-push.sh
+++ build-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VEILID_VERSION=0.1.7
-docker build . --pull --no-cache --build-arg VEILID_VERSION=$VEILID_VERSION --tag ghcr.io/rich0/veilid:$VEILID_VERSION && docker push ghcr.io/rich0/veilid:$VEILID_VERSION
+docker build . --pull --no-cache --build-arg VEILID_VERSION=$VEILID_VERSION --tag ghcr.io/rich0/veilid:$VEILID_VERSION --tag ghcr.io/rich0/veilid:latest && docker push ghcr.io/rich0/veilid:$VEILID_VERSION && docker push ghcr.io/rich0/veilid:latest
