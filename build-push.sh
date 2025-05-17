e504582fbbe5867649caebd209700a7154b72108
Richard Freeman <rich0@gentoo.org>
2025-05-17 03:29:24 -0400

    Veild bump.
    Signed-off-by: Richard Freeman <rich0@gentoo.org>

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VEILID_VERSION=0.4.4
+VEILID_VERSION=0.4.6
 docker build . --pull --no-cache --build-arg VEILID_VERSION=$VEILID_VERSION --tag ghcr.io/rich0/veilid:$VEILID_VERSION --tag ghcr.io/rich0/veilid:latest && docker push ghcr.io/rich0/veilid:$VEILID_VERSION && docker push ghcr.io/rich0/veilid:latest
 #VEILID_VERSION=0.1.7
 #docker build . --pull --no-cache --build-arg VEILID_VERSION=$VEILID_VERSION --tag ghcr.io/rich0/veilid:$VEILID_VERSION && docker push ghcr.io/rich0/veilid:$VEILID_VERSION 
